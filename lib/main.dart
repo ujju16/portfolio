1ee08cfef1d9c8dc1482ef45c0bc0b338710c96a
ujju16 <jul.darrouza@gmail.com>
2024-03-10 04:23:10 +0100

    Ajout d'un bouton et refactor text

--- lib/main.dart
+++ lib/main.dart
@@ -27,21 +27,47 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('Idée Du Jour:'),
-          Text(appState.current.asLowerCase),
+          BigCard(pair: pair),
+          ElevatedButton(
+            onPressed: (){
+              appState.getNext();
+          },
+            child: Text('Next'),
+          ),
         ],
       ),
     );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(pair.asLowerCase);
   }
 }
 
